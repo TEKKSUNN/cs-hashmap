45d6ac1893d0994087a25b4243544668e1d8de18
turwokts <48319429+TEKKSUNN@users.noreply.github.com>
2024-11-08 15:42:16 +0800

    tests: Make tests for HashMap class

--- tests/hashMap.test.js
+++ tests/hashMap.test.js
@@ -1,68 +1,73 @@
 import HashMap from "../src/modules/hashMap";
 
-describe('HashMap', () => {
+describe("HashMap", () => {
   let map;
 
   beforeEach(() => {
     map = new HashMap();
   });
 
-  test('set and get values', () => {
-    map.set('foo', 'bar');
-    expect(map.get('foo')).toBe('bar');
+  test("set and get values", () => {
+    map.set("foo", "bar");
+    expect(map.get("foo")).toBe("bar");
 
-    map.set('foo', 'baz'); // overwrite
-    expect(map.get('foo')).toBe('baz');
+    map.set("foo", "baz"); // overwrite
+    expect(map.get("foo")).toBe("baz");
   });
 
-  test('has method', () => {
-    map.set('key1', 'value1');
-    expect(map.has('key1')).toBe(true);
-    expect(map.has('key2')).toBe(false);
+  test("has method", () => {
+    map.set("key1", "value1");
+    expect(map.has("key1")).toBe(true);
+    expect(map.has("key2")).toBe(false);
   });
 
-  test('remove method', () => {
-    map.set('key1', 'value1');
-    map.set('key2', 'value2');
-    
-    expect(map.remove('key1')).toBe(true);
-    expect(map.has('key1')).toBe(false);
-    expect(map.get('key1')).toBe(null);
+  test("remove method", () => {
+    map.set("key1", "value1");
+    map.set("key2", "value2");
 
-    expect(map.remove('nonExistentKey')).toBe(false);
+    expect(map.remove("key1")).toBe(true);
+    expect(map.has("key1")).toBe(false);
+    expect(map.get("key1")).toBe(null);
+
+    expect(map.remove("nonExistentKey")).toBe(false);
   });
 
-  test('length method', () => {
+  test("length method", () => {
     expect(map.length()).toBe(0);
-    map.set('key1', 'value1');
-    map.set('key2', 'value2');
+    map.set("key1", "value1");
+    map.set("key2", "value2");
     expect(map.length()).toBe(2);
   });
 
-  test('clear method', () => {
-    map.set('key1', 'value1');
-    map.set('key2', 'value2');
+  test("clear method", () => {
+    map.set("key1", "value1");
+    map.set("key2", "value2");
     map.clear();
     expect(map.length()).toBe(0);
-    expect(map.get('key1')).toBe(null);
-    expect(map.get('key2')).toBe(null);
+    expect(map.get("key1")).toBe(null);
+    expect(map.get("key2")).toBe(null);
   });
 
-  test('keys method', () => {
-    map.set('key1', 'value1');
-    map.set('key2', 'value2');
-    expect(map.keys()).toEqual(expect.arrayContaining(['key1', 'key2']));
+  test("keys method", () => {
+    map.set("key1", "value1");
+    map.set("key2", "value2");
+    expect(map.keys()).toEqual(expect.arrayContaining(["key1", "key2"]));
   });
 
-  test('values method', () => {
-    map.set('key1', 'value1');
-    map.set('key2', 'value2');
-    expect(map.values()).toEqual(expect.arrayContaining(['value1', 'value2']));
+  test("values method", () => {
+    map.set("key1", "value1");
+    map.set("key2", "value2");
+    expect(map.values()).toEqual(expect.arrayContaining(["value1", "value2"]));
   });
 
-  test('entries method', () => {
-    map.set('key1', 'value1');
-    map.set('key2', 'value2');
-    expect(map.entries()).toEqual(expect.arrayContaining([['key1', 'value1'], ['key2', 'value2']]));
+  test("entries method", () => {
+    map.set("key1", "value1");
+    map.set("key2", "value2");
+    expect(map.entries()).toEqual(
+      expect.arrayContaining([
+        ["key1", "value1"],
+        ["key2", "value2"],
+      ]),
+    );
   });
 });
